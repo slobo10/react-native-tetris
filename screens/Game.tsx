0cab28744cd60c290d05b2e46530fc681dc263b1
slobo10 <97814567+slobo10@users.noreply.github.com>
2024-02-06 17:00:23 -0800

    Create basic GameContext

--- screens/Game.tsx
+++ screens/Game.tsx
@@ -1,20 +1,29 @@
-import { Component, ReactNode } from "react";
+import { Component, Context, ReactNode, createContext } from "react";
 import Svg from "react-native-svg";
 import styles from "../constants/styles";
 
+let GameContext: Context<{}> = createContext({});
+
 class Game extends Component {
-  private gameConstants: { screenDim: [number, number]; blockSize: number } = {
-    screenDim: [10, 20],
-    blockSize: 30,
-  };
+  private gameContextValue: { screenDim: [number, number]; blockSize: number } =
+    {
+      screenDim: [10, 20],
+      blockSize: 30,
+    };
 
   public render(): ReactNode {
     return (
-      <Svg
-        width={this.gameConstants.screenDim[0] * this.gameConstants.blockSize}
-        height={this.gameConstants.screenDim[1] * this.gameConstants.blockSize}
-        style={styles.gameSvg}
-      ></Svg>
+      <GameContext.Provider value={this.gameContextValue}>
+        <Svg
+          width={
+            this.gameContextValue.screenDim[0] * this.gameContextValue.blockSize
+          }
+          height={
+            this.gameContextValue.screenDim[1] * this.gameContextValue.blockSize
+          }
+          style={styles.gameSvg}
+        ></Svg>
+      </GameContext.Provider>
     );
   }
 }
